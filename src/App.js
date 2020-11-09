e5cd8d8568a1f90fc244f35cc2b148f76cfa6e62
ri3habh <64708363+ri3habh@users.noreply.github.com>
2020-11-08 19:39:44 -0500

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,414 @@
+import React from 'react';
+// import logo from './logo.svg';
+import './App.css';
+import Portfolio from './components/Portfolio'
+function App() {
+  const portfolioLinks = [
+    {
+      title: 'Threads',
+      caption: 'Illustration'
+    },
+    {
+      title: 'Explore',
+      caption: 'Graphic Design'
+    },
+    {
+      title: 'Finish',
+      caption: 'Identity'
+    },
+    {
+      title: 'Lines',
+      caption: 'Branding'
+    },
+    {
+      title: 'Southwest',
+      caption: 'Website Design'
+    },
+    {
+      title: 'Window',
+      caption: 'Photography'
+    },
+    {
+      title: 'Pizza',
+      caption: 'I love pizza!'
+    }
+  ]
+  
+
+  return (
+    <div className="App">
+      <nav className="navbar navbar-expand-lg navbar-dark fixed-top" id="mainNav">
+    <div className="container">
+      <a className="navbar-brand js-scroll-trigger" href="#page-top">Start Bootstrap</a>
+      <button className="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+        Menu
+        <i className="fa fa-bars"></i>
+      </button>
+      <div className="collapse navbar-collapse" id="navbarResponsive">
+        <ul className="navbar-nav text-uppercase ml-auto">
+          <li className="nav-item">
+            <a className="nav-link js-scroll-trigger" href="#services">Services</a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link js-scroll-trigger" href="#portfolio">Portfolio</a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link js-scroll-trigger" href="#about">About</a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link js-scroll-trigger" href="#team">Team</a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link js-scroll-trigger" href="#contact">Contact</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+
+  
+  <header className="masthead">
+    <div className="container">
+      <div className="intro-text">
+        <div className="intro-lead-in">On one of the biggest Health Diseases</div>
+        <div className="intro-heading">Child Obesity</div>
+        <a className="btn btn-primary btn-xl text-uppercase js-scroll-trigger" href="#services">Tell Me More</a>
+      </div>
+    </div>
+  </header>
+      
+  <section className="page-section" id="services">
+    <div className="container">
+      <div className="row">
+        <div className="col-lg-12 text-center">
+          <h2 className="section-heading text-uppercase">What is Obesity?</h2>
+          <h3 className="section-subheading text-muted">Obesity is a chronic disease that is caused by mankind itself. It is caused by an excess of accumulated fat, that your body cannot utilize in physical activities. </h3>
+        </div>
+      </div>
+      <div className="row text-center">
+        <div className="col-md-4">
+          <span className="fa-stack fa-4x">
+            <i className="fa fa-circle fa-stack-2x text-primary"></i>
+            <i className="fa fa-shopping-cart fa-stack-1x fa-inverse"></i>
+          </span>
+          <h4 className="service-heading">Fitness</h4>
+          <p className="text-muted">Often the reason that people get an excess of fat is becuase they normally don't have the time or energy to go exercise and use the energy that they got from the food, causing the fat to be stored in the body.</p>
+        </div>
+        <div className="col-md-4">
+          <span className="fa-stack fa-4x">
+            <i className="fa fa-circle fa-stack-2x text-primary"></i>
+            <i className="fa fa-laptop fa-stack-1x fa-inverse"></i>
+          </span>
+          <h4 className="service-heading">Eating Habits</h4>
+          <p className="text-muted">Eating an excess of fatty acids and foods with high amounts of saturated fats often cause the reason for fat build ups.</p>
+        </div>
+        <div className="col-md-4">
+          <span className="fa-stack fa-4x">
+            <i className="fa fa-circle fa-stack-2x text-primary"></i>
+            <i className="fa fa-lock fa-stack-1x fa-inverse"></i>
+          </span>
+          <h4 className="service-heading">Family Genetics</h4>
+          <p className="text-muted">The rarest reason for Obesity is family Genetics. Sometimes, streams of families may be more prone to being Obese than other families.</p>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  
+
+  
+  <section className="page-section" id="about">
+    <div className="container">
+      <div className="row">
+        <div className="col-lg-12 text-center">
+          <h2 className="section-heading text-uppercase">The Generation of Obesity</h2>
+          <h3 className="section-subheading text-muted">From where the deadly disease of saturated fat started.</h3>
+        </div>
+      </div>
+      <div className="row">
+        <div className="col-lg-12">
+          <ul className="timeline">
+            <li>
+              <div className="timeline-image">
+                <img className="rounded-circle img-fluid" src="assets/img/obesity-1.jpg" alt=""/>
+              </div>
+              <div className="timeline-panel">
+                <div className="timeline-heading">
+                  <h4>2000 Years Ago</h4>
+                  <h4 className="subheading">Myths</h4>
+                </div>
+                <div className="timeline-body">
+                  <p className="text-muted">2,000 years ago, the Greek physician and philosopher Galen diagnosed “bad humors” as the cause of obesity, and prescribed massage, baths, and “slimming foods” like greens, garlic, and wild game for his overweight patients.</p>
+                </div>
+              </div>
+            </li>
+            <li className="timeline-inverted">
+              <div className="timeline-image">
+                 <i class="fas fa-weight"></i>
+                 <img className="rounded-circle img-fluid" src="assets/img/obesity-1.jpg" alt=""/>
+              </div>t
+              <div className="timeline-panel">
+                <div className="timeline-heading">
+                  <h4>1950</h4>
+                  <h4 className="subheading">Discovery</h4>
+                </div>
+                <div className="timeline-body">
+                  <p className="text-muted"> Shortly after WWII, a group of scientists in The Jackson Laboratory, Maine found out a strain of mice, called Ob or OBESITY, which had an excess of fat. </p>
+                </div>
+              </div>
+            </li>
+            <li>
+              <div className="timeline-image">
+                <img className="rounded-circle img-fluid" src="img/about/3.jpg" alt=""/>
+              </div>
+              <div className="timeline-panel">
+                <div className="timeline-heading">
+                  <h4>1950-1970</h4>
+                  <h4 className="subheading">Getting Started</h4>
+                </div>
+                <div className="timeline-body">
+                  <p className="text-muted">Ever since the discovery of Obesity, an outbreak called the Obesity Epidemic has been happening, causing stress levels across the world to be rising very fast.</p>
+                </div>
+              </div>
+            </li>
+            <li className="timeline-inverted">
+              <div className="timeline-image">
+                <img className="rounded-circle img-fluid" src="img/about/4.jpg" alt=""/>
+              </div>
+              <div className="timeline-panel">
+                <div className="timeline-heading">
+                  <h4>1970</h4>
+                  <h4 className="subheading">Fast Rising</h4>
+                </div>
+                <div className="timeline-body">
+                  <p className="text-muted">In 1950, the Obesity rates were at a suprisingly high rate at 10% of the worlds adult population. In only 2 decades, the percent went from 10 to 15%</p>
+                </div>
+              </div>
+            </li>
+            <li className="timeline-inverted">
+              <div className="timeline-image">
+                <img className="rounded-circle img-fluid" src="img/about/2.jpg" alt=""/>
+              </div>t
+              <div className="timeline-panel">
+                <div className="timeline-heading">
+                  <h4>2020</h4>
+                  <h4 className="subheading">Current Crisis</h4>
+                </div>
+                <div className="timeline-body">
+                  <p className="text-muted"> Now, over  40% of all adults have been through some sort of Obesity.</p>
+                </div>
+              </div>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  
+  <section className="bg-light page-section" id="team">
+    <div className="container">
+      <div className="row">
+        <div className="col-lg-12 text-center">
+          <h2 className="section-heading text-uppercase">Credits</h2>
+          <h3 className="section-subheading text-muted">Group Members</h3>
+        </div>
+      </div>
+      <div className="row">
+        <div className="col-sm-4">
+          <div className="team-member">
+            <img className="mx-auto rounded-circle" src="img/team/1.jpg" alt=""/>
+            <h4>Akif Guven</h4>
+            <p className="text-muted"></p>
+            <ul className="list-inline social-buttons">
+              <li className="list-inline-item">
+                <a href="#something">
+                  <i className="fa fa-twitter"></i>
+                </a>
+              </li>
+              <li className="list-inline-item">
+                <a href="#something">
+                  <i className="fa fa-facebook-f"></i>
+                </a>
+              </li>
+              <li className="list-inline-item">
+                <a href="#something">
+                  <i className="fa fa-linkedin-in"></i>
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div className="col-sm-4">
+          <div className="team-member">
+            <img className="mx-auto rounded-circle" src="img/team/2.jpg" alt=""/>
+            <h4>Pranav Sharma</h4>
+            <p className="text-muted"></p>
+            <ul className="list-inline social-buttons">
+              <li className="list-inline-item">
+                <a href="#something">
+                  <i className="fa fa-twitter"></i>
+                </a>
+              </li>
+              <li className="list-inline-item">
+                <a href="#something">
+                  <i className="fa fa-facebook-f"></i>
+                </a>
+              </li>
+              <li className="list-inline-item">
+                <a href="#something">
+                  <i className="fa fa-linkedin-in"></i>
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div className="col-sm-4">
+          <div className="team-member">
+            <img className="mx-auto rounded-circle" src="img/team/3.jpg" alt=""/>
+            <h4>Muhaymin Malik</h4>
+            <p className="text-muted"></p>
+            <ul className="list-inline social-buttons">
+              <li className="list-inline-item">
+                <a href="#something">
+                  <i className="fa fa-twitter"></i>
+                </a>
+              </li>
+              <li className="list-inline-item">
+                <a href="#something">
+                  <i className="fa fa-facebook-f"></i>
+                </a>
+              </li>
+              <li className="list-inline-item">
+                <a href="#something">
+                  <i className="fa fa-linkedin-in"></i>
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+      <div className="row">
+        <div className="col-lg-8 mx-auto text-center">
+        </div>
+      </div>
+    </div>
+  </section>
+
+  
+  <section className="py-5">
+    <div className="container">
+      <div className="row">
+        <div className="col-md-3 col-sm-6">
+          <a href="#something">
+            <img className="img-fluid d-block mx-auto" src="assets/img/obesity-1.jpg" alt=""/>
+          </a>
+        </div>
+        <div className="col-md-3 col-sm-6">
+          <a href="#something">
+            <img className="img-fluid d-block mx-auto" src="img/logos/designmodo.jpg" alt=""/>
+          </a>
+        </div>
+        <div className="col-md-3 col-sm-6">
+          <a href="#something">
+            <img className="img-fluid d-block mx-auto" src="img/logos/themeforest.jpg" alt=""/>
+          </a>
+        </div>
+        <div className="col-md-3 col-sm-6">
+          <a href="#something">
+            <img className="img-fluid d-block mx-auto" src="img/logos/creative-market.jpg" alt=""/>
+          </a>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  
+  <section className="page-section" id="contact">
+    <div className="container">
+      <div className="row">
+        <div className="col-lg-12 text-center">
+          <h2 className="section-heading text-uppercase">BMI CHECKER</h2>
+          <h3 className="section-subheading text-muted">BMI(Body Mass Index) is a system that scientists use to calculate if you are living a healthy lifestyle, or how far off you are. </h3>
+        </div>
+      </div>
+      <div className="row">
+        <div className="col-lg-12">
+          <form id="contactForm" name="sentMessage" novalidate="novalidate">
+            <div className="row">
+              <div className="col-md-6">
+                <div className="form-group">
+                  <input className="form-control" id="name" type="text" placeholder="Your Name *" required="required" data-validation-required-message="Please enter your name."/>
+                  <p className="help-block text-danger"></p>
+                </div>
+                <div className="form-group">
+                  <input className="form-control" id="email" type="email" placeholder="Your Email *" required="required" data-validation-required-message="Please enter your email address."/>
+                  <p className="help-block text-danger"></p>
+                </div>
+                <div className="form-group">
+                  <input className="form-control" id="phone" type="tel" placeholder="Your Phone *" required="required" data-validation-required-message="Please enter your phone number."/>
+                  <p className="help-block text-danger"></p>
+                </div>
+              </div>
+              <div className="col-md-6">
+                <div className="form-group">
+                  <textarea className="form-control" id="message" placeholder="Your Message *" required="required" data-validation-required-message="Please enter a message."></textarea>
+                  <p className="help-block text-danger"></p>
+                </div>
+              </div>
+              <div className="clearfix"></div>
+              <div className="col-lg-12 text-center">
+                <div id="success"></div>
+                <button id="sendMessageButton" className="btn btn-primary btn-xl text-uppercase" type="submit">GET BMI RESULTS</button>
+              </div>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  
+  <footer className="footer">
+    <div className="container">
+      <div className="row align-items-center">
+        <div className="col-md-4">
+          <span className="copyright">Copyright &copy; Pranav Sharma Website 2020</span>
+        </div>
+        <div className="col-md-4">
+          <ul className="list-inline social-buttons">
+            <li className="list-inline-item">
+              <a href="#something">
+                <i className="fa fa-twitter"></i>
+              </a>
+            </li>
+            <li className="list-inline-item">
+              <a href="#something">
+                <i className="fa fa-facebook-f"></i>
+              </a>
+            </li>
+            <li className="list-inline-item">
+              <a href="#something">
+                <i className="fa fa-linkedin-in"></i>
+              </a>
+            </li>
+          </ul>
+        </div>
+        <div className="col-md-4">
+          <ul className="list-inline quicklinks">
+            <li className="list-inline-item">
+              <a href="#something">Privacy Policy</a>
+            </li>
+            <li className="list-inline-item">
+              <a href="#something">Terms of Use</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </footer>
+    </div>
+  );
+}
+
+export default App;
